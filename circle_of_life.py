f9e91ef898eaef1c8c88e73a4f1c0c3c36912de7
SeokHyun Moon <seokhyunmoon@yonsei.ac.kr>
2023-05-08 17:11:12 +0900

    commit

--- circle_of_life.py
+++ circle_of_life.py
@@ -7,13 +7,13 @@ class CircleOfLife:
     def __init__(self, world_size, num_zebras, num_lions):
         self.occupancy = [[False for _ in range(world_size)]
                           for _ in range(world_size)]
-    print_TODO('get random empty coordinates')
-    self.zebras = [Animal(0, 0) for _ in range(num_zebras)]
-    self.lions = [Animla(0, 0) for _ in range(num_lions)]
-    print('Welcome to AIE Safari!')
-    print(f'\tworld size = {world_size}')
-    print(f'\tnumber of zebras = {len(self.zebras)}')
-    print(f'\tnumber of lions = {len(self.lions)}')
+        print_TODO('get random empty coordinates')
+        self.zebras = [Animal(0, 0) for _ in range(num_zebras)]
+        self.lions = [Animal(0, 0) for _ in range(num_lions)]
+        print('Welcome to AIE Safari!')
+        print(f'\tworld size = {world_size}')
+        print(f'\tnumber of zebras = {len(self.zebras)}')
+        print(f'\tnumber of lions = {len(self.lions)}')
 
 if __name__ == '__main__':
     safari = CircleOfLife(5,5,2)
