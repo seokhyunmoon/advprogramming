dd129d01e0fb3d6640d7ff513f51801df9095f18
SeokHyun Moon <seokhyunmoon@yonsei.ac.kr>
2023-05-08 17:09:55 +0900

    commit

---
+++ circle_of_life.py
@@ -0,0 +1,19 @@
+from animal import Animal
+
+def print_TODO(todo):
+    print(f'<<< NOT IMPLEMENTED: {todo} >>>')
+
+class CircleOfLife:
+    def __init__(self, world_size, num_zebras, num_lions):
+        self.occupancy = [[False for _ in range(world_size)]
+                          for _ in range(world_size)]
+    print_TODO('get random empty coordinates')
+    self.zebras = [Animal(0, 0) for _ in range(num_zebras)]
+    self.lions = [Animla(0, 0) for _ in range(num_lions)]
+    print('Welcome to AIE Safari!')
+    print(f'\tworld size = {world_size}')
+    print(f'\tnumber of zebras = {len(self.zebras)}')
+    print(f'\tnumber of lions = {len(self.lions)}')
+
+if __name__ == '__main__':
+    safari = CircleOfLife(5,5,2)
