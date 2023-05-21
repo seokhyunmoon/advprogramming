3e1d9f6ed73de19b27c5c4449e0cb6656f62e3f8
SeokHyun Moon <seokhyunmoon@yonsei.ac.kr>
2023-05-21 19:18:39 +0900

    seokhyun branch commit

--- circle_of_life.py
+++ circle_of_life.py
@@ -5,35 +5,31 @@ from utils import print_TODO
 class CircleOfLife:
 
     def __init__(self, world_size, num_zebras, num_lions):
-        self.grid = [['.' for _ in range(world_size)]
-                          for _ in range(world_size)]
-
         self.world_size = world_size
-        self.reset_grid()
-        print_TODO('get random empty coordinates')
         self.zebras = [Zebra(0, 0) for _ in range(num_zebras)]
         self.lions = [Lion(0, 0) for _ in range(num_lions)]
+        self.update_grid()
         self.timestep = 0
+        print_TODO('get random empty coordinates')
         print('Welcome to AIE Safari!')
         print(f'\tworld size = {world_size}')
         print(f'\tnumber of zebras = {len(self.zebras)}')
         print(f'\tnumber of lions = {len(self.lions)}')
     
-    def reset_grid(self):
-        self.grid = [['.' for _ in range(self.world_size)]for _ in range(self.world_size)]
-
-    def display(self):
-        print(f'Clock: {self.timestep}')
-        top_coord_str = ' '.join([f'{coord}' for coord in range(len(self.grid))])
-        print('   ' + top_coord_str)
-        self.reset_grid()
+    def update_grid(self):
+        self.grid = [['.' for _ in range(self.world_size)]
+                          for _ in range(self.world_size)]
         for animal in self.zebras:
             self.grid[animal.y][animal.x] = 'Z'
         for animal in self.lions:
             self.grid[animal.y][animal.x] = 'L'
-        for row, line in enumerate(self.grid):
-            print(f'{row:2} ' + ' '.join(line))
 
+    def display(self):
+        print(f'Clock: {self.timestep}')
+        top_coord_str = ' '.join([f'{coord + 1:3}' for coord in range(len(self.grid))])
+        print('   ' + top_coord_str)
+        for row, line in enumerate(self.grid):
+            print(f'{ row + 1:3} ' + ' '.join(f'{cell:3}' for cell in line))
         key = input('enter [q] to quit:')
         if key == 'q':
             exit()
@@ -42,24 +38,13 @@ class CircleOfLife:
         print_TODO('step_move()')
         for zebra in self.zebras:
             zebra.move(self.grid)
+            self.update_grid()
         for lion in self.lions:
             lion.move(self.grid)
+            self.update_grid
     
     def step_breed(self):
         print_TODO('step_breed()')
-        # for animal in self.zebras + self.lions:
-        #     print_TODO('get empty neighbor')
-        #     x, y = 0, 0
-        #     animal.breed(x, y)
-
-        # for zebra in self.zebras:
-        #     zebra.breed(self.grid)
-        # for lion in self.lions:
-        #     lion.breed(self.grid)
-    
-    # def step_age(self):
-    #     for zebra in self.zebras:
-    #         zebra.age += 1
 
     
     def run(self, num_timesteps=100):
@@ -68,10 +53,6 @@ class CircleOfLife:
             self.timestep +=1
             self.step_move()
             self.display()
-
-            #동물 나이 +1
-            # self.step_age()
-            # self.display()
 
             self.step_breed()
             self.display()
