0ab400e74b22a8a50bdfccaeac5f3409e5c8db86
kimiyeon <miyeoni@yonsei.ac.kr>
2023-05-22 16:41:20 +0900

    coord system change y,x

--- animal.py
+++ animal.py
@@ -2,7 +2,7 @@ import random
 from utils import print_TODO
 
 class Animal:
-    def __init__(self, x, y):
+    def __init__(self, y, x):
         self.x = x
         self.y = y
         self.age = 0
@@ -12,30 +12,36 @@ class Animal:
         world_width = len(grid[0])
         x, y = self.x, self.y
         neighbors = []
-        neighbors.append([x - 1, y])
-        neighbors.append([x + 1, y])
-        neighbors.append([x, y - 1])
-        neighbors.append([x, y + 1])
+        neighbors.append([y - 1, x])
+        neighbors.append([y + 1, x])
+        neighbors.append([y, x - 1])
+        neighbors.append([y, x + 1])
         neighbors_valid = [neighbor for neighbor in neighbors
                            if neighbor[0] >= 0
                            and neighbor[0] < world_width
                            and neighbor[1] >= 0
                            and neighbor[1] < world_height
-                           and grid[neighbor[1][neighbor[0] == target]]]
+                           and str(grid[neighbor[1][neighbor[0] == target]])]
         return neighbors_valid
 
     def move_to(self, grid, target) -> bool:
         neighbors = self.get_neighbors(grid, target)
         if len(neighbors) > 0:
+            grid[self.y][self.x] = Empty(self.y, self.x)
             chosen_neighbor = random.choice(neighbors)
-            self.x, self.y = chosen_neighbor
+            self.y, self.x = chosen_neighbor
+            grid[self.y][self.x] = self
             return True
         else:
             return False
 
     def breed(self, x, y):
         return Animal(x, y)
-    
+
+class Empty(Animal):
+    def __str__(self):
+        return '.' 
+
 class Zebra(Animal):
     def __str__(self):
         return 'Z'
@@ -50,6 +56,9 @@ class Zebra(Animal):
         
 
 class Lion(Animal):
+    def __str__(self):
+        return 'L'
+    
     def move(self, grid):
         hunt_is_successful = self.move_to(grid, target='Z')
         if hunt_is_successful:
