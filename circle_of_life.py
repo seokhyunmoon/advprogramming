9a86b0b2f7b31153c06d12e0dd244bb24b05bb44
SeokHyun Moon <seokhyunmoon@yonsei.ac.kr>
2023-05-22 16:38:59 +0900

    get_random_coords

--- circle_of_life.py
+++ circle_of_life.py
@@ -6,6 +6,8 @@ class CircleOfLife:
 
     def __init__(self, world_size, num_zebras, num_lions):
         self.world_size = world_size
+        random.seed(0)
+        zebra_coord, lion_coords = self.get_random_coords(num_zebras, num_lions)
         self.zebras = [Zebra(0, 0) for _ in range(num_zebras)]
         self.lions = [Lion(0, 0) for _ in range(num_lions)]
         self.update_grid()
@@ -15,6 +17,14 @@ class CircleOfLife:
         print(f'\tworld size = {world_size}')
         print(f'\tnumber of zebras = {len(self.zebras)}')
         print(f'\tnumber of lions = {len(self.lions)}')
+    
+    def get_random_coords(self, num_zebras, num_lions):
+        all_coords = [(x,y) for y in range(self.world_size)
+                      for x in range(self.world_size)]
+        zebra_coords = random.sample(all_coords, num_zebras)
+        all_coords= list(set(all_coords) - set(zebra_coords))
+        lion_coords = random.sample(all_coords, num_lions)
+        return zebra_coords, lion_coords
     
     def update_grid(self):
         self.grid = [['.' for _ in range(self.world_size)]
